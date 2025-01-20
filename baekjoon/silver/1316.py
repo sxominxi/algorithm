1415252fb8ec928d557d7e75d4c26ed498954a00
minji <s__min_ji@naver.com>
2025-01-21 00:00:50 +0900

    250120

--- baekjoon/silver/1316.py
+++ baekjoon/silver/1316.py
@@ -1,8 +1,25 @@
 N = int(input())
 cnt = 0
 
+plag = True
 for n in range(0, N):
-    w = list(input())
+    w = set(input())
+    
+    if len(w) == 1:
+        cnt += 1
+        break
 
-    for i in range(len(w)):
-        w.w[i]
+    else:
+        for word in w:
+            idx = []
+            for i in range(len(N)):
+                if N[i] == word:
+                    idx.append(i)
+            for j in range(len(idx)-1):
+                plag = False
+                break
+        
+        if plag == True:
+            cnt += 1
+
+print(cnt)
