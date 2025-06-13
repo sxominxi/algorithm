c5dacf0d4876feebe86998a672d7750d48c8b0a1
minji <s__min_ji@naver.com>
2025-06-13 23:54:57 +0900

    250613

--- baekjoon/bronze/10870.py
+++ baekjoon/bronze/10870.py
@@ -1,9 +1,9 @@
-N = int(input())
+N = int(input()) # 입력받기
 
-def fibo(n):
-  if n < 2:
-    return n
-  else:
-    return fibo(n-1) + fibo(n-2)
+def fibo(n): # 피보나치 함수
+  if n < 2: # 1번째 피보나치 수 까지는
+    return n # 그대로 반환환
+  else: # 그 이후부터는 
+    return fibo(n-1) + fibo(n-2) # 바로 앞 두 피보나치 수의 합을 return
 
 print(fibo(N))
