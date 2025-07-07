92532a4d543dd9448c7d76108241dd2cb1c1807f
minji <s__min_ji@naver.com>
2025-07-07 21:59:50 +0900

    250707

--- baekjoon/silver/1912.py
+++ baekjoon/silver/1912.py
@@ -4,3 +4,10 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 
+dp = [0] * N
+dp[0] = arr[0] # 0번 인덱스 까지의 최대합은 arr[0]
+
+for i in range(1, N):
+  dp[i] = max(arr[i], dp[i-1]+arr[i]) # 이전까지의 합이 더 크면 더한거, 아니면 새로
+
+print(max(dp))
